04ddf87d86808656d631b5cb29898c95f734c247
Waveweb <waveweb@Wavewebs-MBP.Home>
2024-03-18 15:39:27 +0000

    localhost and live be url set

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,15 @@
 import axios, { AxiosResponse } from 'axios';
 
 // Base URL of your API
-const baseUrl = 'http://localhost:3000';
+const LOCAL_URL = 'http://localhost:3000';
+const LIVE_URL='https://59e29064-e195-40f0-a55d-218350dcec90-00-1tv5qg129s2kd.janeway.replit.dev';
+
+// Check if "localhost" is present in the window's href
+const isLocalhost = window.location.href.includes('localhost');
+
+// Set the base URL based on the condition
+const baseUrl = isLocalhost ? LOCAL_URL : LIVE_URL;
+
 export const schedules= 'schedules';
 export const scheduleLogs=(scheduleId:string)=>'scheduleLogs?scheduleId'+scheduleId;
 
